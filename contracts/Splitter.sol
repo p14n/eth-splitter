4d6e819cab9364488dabf9313ff65fcddd487595
Dean Chapman <dean@p14n.com>
2018-04-11 22:51:21 +0100

    We have a trio

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,10 +2,16 @@ pragma solidity ^0.4.17;
 
 contract Splitter {
 
-         address public alice;
+  address public owner;
+  address public recipient1;
+  address public recipient2;
 
-         function Splitter() public {
-                alice = msg.sender;
-         }
+  function Splitter(
+    address firstrecipient,
+    address secondrecipient) public {
 
+    owner = msg.sender;
+    recipient1 = firstrecipient;
+    recipient2 = secondrecipient;
+  }
 }
--- test/splitter.js
+++ test/splitter.js
@@ -3,10 +3,16 @@ var Splitter = artifacts.require("./Splitter.sol");
 contract('Splitter', function ([alice,bob,carol]) {
     let splitter;
     beforeEach('setup contract for each test', async () => {
-        splitter = await Splitter.new()
+        splitter = await Splitter.new(bob,carol)
     })
     it('has an alice', async () => {
-        assert.equal(await splitter.alice(), alice)
+        assert.equal(await splitter.owner(), alice)
+    })
+    it('has a bob', async () => {
+        assert.equal(await splitter.recipient1(), bob)
+    })
+    it('has a carol', async () => {
+        assert.equal(await splitter.recipient2(), carol)
     })
 
 })
