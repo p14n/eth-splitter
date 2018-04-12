59b835f51c12b34fe9dd8faded2e09ff228e72dd
Dean Chapman <dean@p14n.com>
2018-04-12 08:26:33 +0100

    Balances visible

--- test/splitter.js
+++ test/splitter.js
@@ -5,6 +5,8 @@ contract('Splitter', function ([alice,bob,carol]) {
     beforeEach('setup contract for each test', async () => {
         splitter = await Splitter.new(bob,carol)
     })
+
+    //there are 3 people: Alice, Bob and Carol.
     it('has an alice', async () => {
         assert.equal(await splitter.owner(), alice)
     })
@@ -15,4 +17,27 @@ contract('Splitter', function ([alice,bob,carol]) {
         assert.equal(await splitter.recipient2(), carol)
     })
 
+    it('allows visibility of contract balance on web page',async () => {
+
+        assert.equal(await web3.eth.getBalance(splitter.address),0)
+    })
+    it('allows visibility of alice balance on web page',async () => {
+
+        const balance = await web3.eth.getBalance(await splitter.owner());
+        assert.equal(balance.isZero(),false);
+    })
+    it('allows visibility of bob balance on web page',async () => {
+
+        const balance = await web3.eth.getBalance(await splitter.recipient1());
+        assert.equal(balance.isZero(),false);
+    })
+    it('allows visibility of carol balance on web page',async () => {
+
+        const balance = await web3.eth.getBalance(await splitter.recipient2());
+        assert.equal(balance.isZero(),false);
+    })
+    //whenever Alice sends ether to the contract, half of it goes to Bob and the other half to Carol.
+    
+
+//Alice can use the Web page to split her ether.
 })
