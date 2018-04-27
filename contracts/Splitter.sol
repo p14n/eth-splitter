908b9a5e38cb03fcb2565b89462518cb5a91faa5
Dean Chapman <dean@p14n.com>
2018-04-27 22:56:33 +0100

    Review updates

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -7,9 +7,9 @@ contract Splitter {
   bool private killed;
   mapping (address => uint) private balances;
 
-  event SplitEvent(uint amount);
-  event WithdrawEvent(uint amount, address payee);
-    
+  event SplitEvent(address indexed payer,uint amount,address indexed payee1,address indexed payee2);
+  event WithdrawEvent(uint amount, address indexed payee);
+
   function Splitter() public {
     owner = msg.sender;
   }
@@ -20,24 +20,21 @@ contract Splitter {
     require(!killed);
     require(msg.sender == owner);
     require(msg.value % 2 == 0);
-    require(firstRecipient > 0);
-    require(secondRecipient > 0);
+    require(firstRecipient != 0);
+    require(secondRecipient != 0);
     require(firstRecipient != secondRecipient);
 
     uint toPay = msg.value/2;
-    uint newBal1 = newBalanceOfRecipient(toPay,firstRecipient);
-    uint newBal2 = newBalanceOfRecipient(toPay,secondRecipient);
-
-    balances[firstRecipient] = newBal1;
-    balances[secondRecipient] = newBal2;
-    SplitEvent(toPay);
+    updateBalanceOfRecipient(toPay,firstRecipient);
+    updateBalanceOfRecipient(toPay,secondRecipient);
+    SplitEvent(owner,toPay,firstRecipient,secondRecipient);
   }
 
-  function newBalanceOfRecipient(uint toPay,address recipient) private view returns (uint) {
+  function updateBalanceOfRecipient(uint toPay,address recipient) private {
     uint bal = balances[recipient];
     uint newBal = toPay + bal;
     assert(newBal > bal);
-    return newBal;
+    balances[recipient] = newBal;
   }
 
   function withdraw() public {
@@ -45,8 +42,8 @@ contract Splitter {
     uint balance = balances[msg.sender];
     require(balance > 0);
     balances[msg.sender] = 0;
-    msg.sender.transfer(balance);
     WithdrawEvent(balance,msg.sender);
+    msg.sender.transfer(balance);
 
   }
 
